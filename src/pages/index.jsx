ff1005ee349544db364a1179da9f459a2c453156
gui <gui.polezi@hotmail.com>
2026-06-07 20:25:36 -0300

    Creating hero section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,13 @@
 export function Home() {
     return (
-        <div>
-            <p>Ola mundo</p>
+        <div className="flex items-center h-100">
+            <div className="grid grid-cols-2 p-5">
+                <div className="col-span-1">
+                </div>
+                <div className="col-span-1">
+                    <p id="textHome" className="text-start" style={{fontWeight: 'bold'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam efficitur risus ac dolor fermentum lacinia. Ut porta purus et accumsan dignissim. Maecenas laoreet sodales odio. Sed id urna erat. Fusce fermentum mollis metus, id suscipit risus malesuada non. Integer bibendum tellus in sapien tempus volutpat.</p>
+                </div>
+            </div>
         </div>
     )
 }
