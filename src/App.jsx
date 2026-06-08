e6f9d7866338ff9b77140a5ee42d54f4db53c9ce
gui <gui.polezi@hotmail.com>
2026-06-07 22:43:36 -0300

    some adjusts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import AppRoutes from './routes'
+
 function App() {
   
   return (
