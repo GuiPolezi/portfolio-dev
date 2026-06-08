c096715e576de09984547b7297e6ec2a78936371
gui <gui.polezi@hotmail.com>
2026-06-07 21:59:25 -0300

    Adding Stars

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,5 +1,14 @@
 export function Projects() {
     return (
-        <p>Projects</p>
+        <>
+        <div className="headerProjects text-end">
+            <h3 id="titleProject" className="text-5xl">Starred Projects</h3>
+            <div id="stars">
+                <i id="starone" class="fa-solid fa-star"></i>
+                <i id="startwo" class="fa-solid fa-star"></i>
+                <i id="starthree" class="fa-solid fa-star"></i>
+            </div>
+        </div>
+        </>
     )
 }
