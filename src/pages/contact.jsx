71ae105224e26b1b09ca9c03887e4ba64c313495
gui <gui.polezi@hotmail.com>
2026-06-08 21:55:57 -0300

    Form is stylized

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -21,15 +21,17 @@ export function Contact() {
                 </div>
                 <div className="col-span-1 text-end">
                     <h4>Contact</h4>
-                    <div className="grid mt-5 grid-cols-3">
+                    <div className="grid mt-2 grid-cols-3">
                         <div className="col-span-1">
 
                         </div>
                         <div className="col-span-2">
                             <form action="" className="flex flex-col">
                                 <input type="email" placeholder="Mail" className="text-left placeholder-right p-2" required/>
-                            
-                                <button type="submit">Send <i className="fa-solid fa-arrow-right"></i></button>
+                                <textarea name="Message" rows={5} placeholder="Your Message" required className="resize-none mt-5 text-left placeholder-right p-2" id=""></textarea>
+                                <div className="text-center">
+                                    <button type="submit" className="mt-4">Send <i className="fa-solid fa-arrow-right"></i></button>
+                                </div>
                             </form>
                         </div>
                     </div>
