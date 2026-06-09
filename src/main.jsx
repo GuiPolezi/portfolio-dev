f0e3d6ce4f57923236cefdb754a7901fb87f428d
gui <gui.polezi@hotmail.com>
2026-06-08 22:38:19 -0300

    Added a simple loading

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
-   <StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
-  )
+)
+
+
+requestAnimationFrame(() => {
+  const loader = document.getElementById('initial-loader');
+  if (loader) {
+    loader.style.transition = 'opacity 0.3s ease';
+    loader.style.opacity = '0';
+    setTimeout(() => loader.remove(), 300); // remove do DOM após o fade
+  }
+});
