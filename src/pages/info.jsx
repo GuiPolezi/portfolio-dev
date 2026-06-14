ef3b1b7d3a098de04780d3ef63999c17b2ca848d
gui <gui.polezi@hotmail.com>
2026-06-14 11:36:26 -0300

    Responsivity adjusts almost done on Info.jsx

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -4,14 +4,14 @@ export function Info() {
             <div className="grid grid-cols-2">
                 <div className="col-span-1">
                     <h3 id="infoText" className="mb-2">Info</h3>
-                    <p className="font-bold text-justify mb-10" id="infoP">
+                    <p className="font-bold text-justify mb-10 text-xs lg:text-md" id="infoP">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum semper erat, quis aliquam urna maximus rutrum. Integer eget pulvinar sem, ac pretium dui. Suspendisse commodo fringilla arcu in finibus. Sed eros nulla, rutrum vel vehicula ac, lacinia nec nulla. Pellentesque fringilla tempus ornare.
                     </p>
-                    <p className="font-bold text-justify mb-10" id="infoP">
+                    <p className="font-bold text-justify mb-2 lg:mb-10 text-xs lg:text-md" id="infoP">
                        Morbi nec ultrices est. Pellentesque ligula urna, elementum commodo urna sed, interdum faucibus est. Pellentesque a ante eu diam malesuada tincidunt nec id eros. Aliquam eros arcu, tempor quis leo vitae, posuere rhoncus turpis. Curabitur elementum leo eget massa elementum, vel laoreet massa blandit. Aenean mattis vulputate auctor. Sed non ipsum congue, sollicitudin mauris et, porta magna. Suspendisse in urna at purus porta luctus sed at sapien. Curabitur scelerisque imperdiet elit et ultricies.
                     </p>
                     <h3 id="infoText">My CV</h3>
-                    <a id="infoA" href="#">Download <i class="fa-solid fa-arrow-right"></i> </a>
+                    <a id="infoA" className="text-xs" href="#">Download <i class="fa-solid fa-arrow-right"></i> </a>
                 </div>
                 <div id="topics" className="col-span-1 text-end">
                         <p className="text-sm md:text-2xl lg:text-5xl mb-0 lg:mb-5" id="infoTitle">Front-End Developer</p>
